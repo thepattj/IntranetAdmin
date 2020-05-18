be30c2f515b2253e0b6b5b66d6e078caf493ac01
thepattj <jdej261@gmail.com>
2020-05-18 12:50:19 -0500

    Initial commit

---
+++ application/controllers/Prueba.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Prueba extends CI_Controller {
+
+	public function index()
+	{
+		//$this->load->view('welcome_message');
+		echo "SALIO LA PRUEBA";
+	}
+}
